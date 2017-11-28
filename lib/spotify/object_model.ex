bdd558909a1892190ae02cd69547fd3a9347c3e0
chip reed <chip@chip.sh>
2017-11-27 19:33:12 -0800

    add quotes in object model documentation

--- lib/spotify/object_model.ex
+++ lib/spotify/object_model.ex
@@ -1,13 +1,13 @@
 defmodule Spotify.ObjectModel do
   @moduledoc """
-    A behavior for implementing the as callback for Spotify object models
+    A behavior for implementing the `as/0` callback for Spotify object models
     that are returned from the web api.
   """
 
   @doc """
-    Returns each Object Models model for json decoding.
+    Returns each Object Models' model for json decoding.
 
-    This callback is passed into `Poison.decode`'s `:as` option for formatting
+    This callback is passed into `Poison.decode` `:as` option for formatting
     incoming json as structs.  Each Object Model should implement it, and nest
     other Object Models' `as` functions in their own as needed.
   """
